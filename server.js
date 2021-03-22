04ef58a794eabfc35e3c7d32877909ade2078d96
gustavomerini <gutomerini@gmail.com>
2021-03-22 10:45:03 -0300

    try catch

--- server.js
+++ server.js
@@ -1,41 +1,46 @@
 module.exports.init = () => {
-  const express = require("express");
-  const app = express();
-  const path = require("path");
-  const cookieParser = require("cookie-parser");
-  const cors = require("cors");
-  const http = require("http");
+  try {
+    console.log('init server');
+    const express = require("express");
+    const app = express();
+    const path = require("path");
+    const cookieParser = require("cookie-parser");
+    const cors = require("cors");
+    const http = require("http");
 
-  // view engine setup
-  app.set("views", path.join(__dirname, "views"));
-  app.set("view engine", "jade");
+    // view engine setup
+    app.set("views", path.join(__dirname, "views"));
+    app.set("view engine", "jade");
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, "public")));
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, "public")));
 
-  // error handler
-  app.use(function (err, req, res, next) {
-    const message = err.data.status.message;
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // error handler
+    app.use(function (err, req, res, next) {
+      const message = err.data.status.message;
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    if (message) {
-      res.send(message);
-    } else {
-      res.send("error");
-    }
-  });
-  const port = normalizePort(process.env.PORT || "3000");
-  app.set("port", port);
-  const server = http.createServer(app);
-  server.listen(port, () =>
-    console.log(`Node server listening on port ${port}...`)
-  );
+      // render the error page
+      if (message) {
+        res.send(message);
+      } else {
+        res.send("error");
+      }
+    });
+    const port = normalizePort(process.env.PORT || "3000");
+    app.set("port", port);
+    const server = http.createServer(app);
+    server.listen(port, () =>
+      console.log(`Node server listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
